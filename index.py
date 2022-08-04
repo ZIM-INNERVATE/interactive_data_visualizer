77dc8759df9a726b56e1f0cedde55f8c45a13995
Mohammad Wasil <mwasil@outlook.co.id>
2022-08-04 19:23:42 +0200

    add default host, port and debug mode

--- index.py
+++ index.py
@@ -1,14 +1,17 @@
 import os
 
-from dotenv import load_dotenv
-
 from app import app, server
 from callbacks import callbacks
 
-dotenv_path = os.path.join(os.path.dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
-load_dotenv(dotenv_path=dotenv_path, override=True)
 
 if __name__ == '__main__':
+    if not os.environ.get("HOST"):
+        os.environ["HOST"] = "0.0.0.0"
+    if not os.environ.get("PORT"):
+        os.environ["PORT"] = 8085
+    if not os.environ.get("DEBUG"):
+        os.environ["DEBUG"] = False
+
     app.run_server(host = os.environ.get("HOST"),
                    port = int(os.environ.get("PORT")),
                    debug = bool(os.environ.get("DEBUG")))
